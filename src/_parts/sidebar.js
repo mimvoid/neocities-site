4480c3cfabfed44dca8f695e7d54608ead814541
mimvoid <mimvoid@proton.me>
2024-07-26 13:48:33 -0400

    update sidebar design

--- src/_parts/sidebar.js
+++ src/_parts/sidebar.js
@@ -8,12 +8,16 @@ const sidebar = `
       <p><a href="/about">About</a></p>
     </div>
     <div class="box">
-      <h4>Biology</h4>
-      <p><a href="/biology/critter-shoutouts">Critter Shoutouts</a></p>
-      <p><a href="/biology/flora-shoutouts">Flora Shoutouts</a></p>
-      <p><a href="/biology/fungi-shoutouts">Fungi Shoutouts</a></p>
-      <h4>Design</h4>
-      <p><a href="/design/my-favorite-fonts">My Favorite Fonts</a></p>
+      <details>
+        <summary>Biology</summary>
+        <p><a href="/biology/critter-shoutouts">Critter Shoutouts</a></p>
+        <p><a href="/biology/flora-shoutouts">Flora Shoutouts</a></p>
+        <p><a href="/biology/fungi-shoutouts">Fungi Shoutouts</a></p>
+      </details>
+      <details>
+        <summary>Design</summary>
+        <p><a href="/design/my-favorite-fonts">My Favorite Fonts</a></p>
+      </details>
     </div>
     <div class="box">
       <p>Guestbook (TBA!)</p>
