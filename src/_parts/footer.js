c2158e6cac221aa98cd60186ebf12ab1a7c2a899
mimvoid <mimvoid@proton.me>
2024-07-26 13:48:07 -0400

    add credits page

--- src/_parts/footer.js
+++ src/_parts/footer.js
@@ -3,7 +3,7 @@ const fWrapper = document.querySelector('.wrapper')
 const footer = `
 <div class="footer">
   <p>Site by mimvoid © 2024</p>
-  <p><a href="https://github.com/mimvoid/neocities-site">Source Code</a> - <a href="#top">Top</a></p>
+  <p><a href="/credits">Credits</a> - <a href="https://github.com/mimvoid/neocities-site">Source Code</a> - <a href="#top">Top</a></p>
 </div>
 `
 fWrapper.insertAdjacentHTML('beforeend', footer);
