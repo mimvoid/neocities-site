39228dbbdf09739041e0283d00ab808ccf6db480
mimvoid <mimvoid@proton.me>
2024-07-28 20:18:10 -0400

    tui style header & footer

--- src/_parts/footer.js
+++ src/_parts/footer.js
@@ -2,8 +2,35 @@ const fWrapper = document.querySelector('.wrapper')
 
 const footer = `
 <div class="footer">
-  <p>Site by mimvoid © 2024</p>
-  <p><a href="/credits">Credits</a> - <a href="https://github.com/mimvoid/neocities-site">Source Code</a> - <a href="#top">Top</a></p>
+  <div class="bar">
+    <div class="left">
+      <p id="mode">
+        <i class="fa fa-compass"></i>
+        NORMAL
+      </p>
+      <p id="source-code">
+        <a href="https://github.com/mimvoid/neocities-site">
+          <i class="fa fa-code"></i>
+          source code
+        </a>
+      </p>
+      <p id="author">mimvoid © 2024</p>
+    </div>
+    <div class="right">
+      <p id="filetype">html</p>
+      <p id="credits">
+        <a href="/credits">
+        credits
+        </a>
+      </p>
+      <p id="top">
+        <a href="#top">
+          <i class="fa fa-caret-up"></i>
+          Top
+        </a>
+      </p>
+    </div>
+  </div>
 </div>
 `
 fWrapper.insertAdjacentHTML('beforeend', footer);
--- src/_parts/header.js
+++ src/_parts/header.js
@@ -2,7 +2,23 @@ const hWrapper = document.querySelector('.wrapper')
 
 const header = `
 <div class="header">
-  <h1>mimvoid's little corner</h1>
+  <div class="tab">
+    <a href="/">
+      <i class="fa-regular fa-fw fa-folder-open"></i>
+      <p>mimvoid's little corner</p>
+    </a>
+  </div>
+  <div class="actions">
+    <a href="/">
+      <i class="fa fa-fw fa-minus"></i>
+    </a>
+    <a href="/">
+      <i class="fa-regular fa-fw fa-window-restore"></i>
+    </a>
+    <a href="https://neocities.org/">
+      <i class="fa fa-fw fa-xmark"></i>
+    </a>
+  </div>
 </div>
 `
-hWrapper.insertAdjacentHTML('afterbegin', header);
+hWrapper.insertAdjacentHTML('beforebegin', header);
