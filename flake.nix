9a365bbf96afe8c619ad82866d430f097007b014
mimvoid <mimvoid@proton.me>
2024-10-21 16:08:28 -0400

    add prettier go template plugin

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Provides tools for developing my neocities site";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
   outputs = { self, nixpkgs }:
@@ -12,10 +12,12 @@
   {
     devShells."x86_64-linux".default = pkgs.mkShell {
       name = "neocities";
-      packages = with pkgs; [ neocities-cli hugo ];
-      shellHook = ''
-        alias ncp='neocities push --prune'
-      '';
+
+      packages = with pkgs; [
+        neocities-cli
+        hugo
+        prettier-plugin-go-template
+      ];
     };
   };
 }
