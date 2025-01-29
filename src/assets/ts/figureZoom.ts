ba7ed2e289250d0f2709dd436a46329348d50d95
mimvoid <mimvoid@proton.me>
2025-01-29 09:49:53 -0500

    fix layout shifting with zoomed figures

--- src/assets/ts/figureZoom.ts
+++ src/assets/ts/figureZoom.ts
@@ -1,9 +1,16 @@
 const figures = document.querySelectorAll("figure.zoomable");
 
 figures.forEach (figure => {
-  figure.addEventListener("click", (event) => {
-    event.target.matches("img")
-      ? figure.classList.add("zoomed")
-      : figure.classList.remove("zoomed")
+  figure.addEventListener("click", () => {
+    const body = document.querySelector("body");
+
+    const zoomedFigure = figure.cloneNode(true);
+    zoomedFigure.classList.add("zoomed");
+
+    zoomedFigure.addEventListener("click", () => {
+      body.removeChild(zoomedFigure);
+    })
+
+    body.appendChild(zoomedFigure);
   })
 })
