1350b5812f3804f6e5b0aea470614da520e90bf6
mimvoid <mimvoid@proton.me>
2025-02-08 15:25:19 -0500

    remove prefers light theme support since neocities uses the wrong preview
    I didn't want to do this, but my website is first and foremost in dark
    mode, and I didn't want people's first impression to be the light mode.
    It shouldn't be too disruptive, since you would only need to switch to
    light mode once for it to stick.

--- src/assets/ts/themeToggle/main.ts
+++ src/assets/ts/themeToggle/main.ts
@@ -1,20 +1,17 @@
 import { button, setLightTheme, setDarkTheme } from "./utils";
 
-// Get an updated value with each call
-const localTheme = () => localStorage.getItem("theme");
-
-// Set local theme
-if (localTheme() === null) {
-  const prefersLight = window.matchMedia("(prefers-color-scheme: light)").matches;
-  localStorage.setItem("theme", prefersLight ? "light" : "dark");
-}
-
 // Set theme on page enter (dark mode is the default)
-if (localTheme() === "light") setLightTheme()
+const enterTheme = localStorage.getItem("theme");
+
+if (enterTheme === null) {
+  localStorage.setItem("theme", "dark");
+} else if (enterTheme === "light") {
+  setLightTheme()
+}
 
 // Implement the theme switcher
 button?.addEventListener("click", () => {
-  localTheme() === "dark" ? setLightTheme() : setDarkTheme()
+  localStorage.getItem("theme") === "dark" ? setLightTheme() : setDarkTheme()
 
   // Handle lampshade-style animation
   const cord = document.getElementById("theme_toggle_cord");
